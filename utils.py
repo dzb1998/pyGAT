4a6bc1e671c29bc8a9866f64820f374eae111a79
邓朱博 <dengzhubo@vpn-128-97-244-151.host.ucla.edu>
2020-05-16 18:11:57 -0700

    updates

--- utils.py
+++ utils.py
@@ -2,6 +2,11 @@ import numpy as np
 import scipy.sparse as sp
 import torch
 
+import csv
+import unicodedata
+import os
+
+
 
 def encode_onehot(labels):
     classes = set(labels)
@@ -13,6 +18,33 @@ def encode_onehot(labels):
 def load_data(path="./data/cora/", dataset="cora"):
     """Load citation network dataset (cora only for now)"""
     print('Loading {} dataset...'.format(dataset))
+
+    with open('entities.dict') as fin:
+    entity2id = dict()
+    id2entity = dict()
+    for line in fin:
+        eid, entity = line.strip().split('\t')
+        entity2id[entity] = int(eid)
+        id2entity[int(eid)] = entity
+
+    row_1 = []
+    row_1 = []
+
+    with open('output1.csv') as fin:
+        for row in csv.reader(fin, delimiter=','):
+            if row[0] == '':
+                continue
+            # print(row)
+            row_1 = entity2id[row[1]]
+            row_2 = row[2].strip('][').split(', ')
+            # print(row_2)
+            row_2 = [float(i) for i in row_2]
+            print(row_1)
+            print(row_2)
+
+
+
+
 
     idx_features_labels = np.genfromtxt("{}{}.content".format(path, dataset), dtype=np.dtype(str))
     features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
